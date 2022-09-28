f63fd3713ed15d9100d776fa2e6cebb16299eb79
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2022-09-28 19:39:28 +0100

    Install npm, Complete exercises 01,02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, number) {
+    let wordRepeat = "";
+    if (number < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < number; i++) {
+        wordRepeat += word;
+    }
+    return wordRepeat;
 };
 
 // Do not edit below this line
